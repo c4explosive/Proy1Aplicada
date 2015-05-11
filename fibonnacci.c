c90f4fe86ea8a5ae216415a33674cae8a61f0188
c4explosive <ao946391@gmail.com>
2015-05-11 15:56:31 -0500

    fib

---
+++ fibonnacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define clrscr() system("clear")
+void fibo(int n1);
+void main()
+{
+    fibo(0,max);
+}
+void fibo(int n1,int)
+{
+    	printf("%d,",n1);
+    	fibo(n1,n2);
+}
