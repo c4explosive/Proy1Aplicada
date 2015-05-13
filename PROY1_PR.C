f01228009255fb308b81c511120cebb0a8e9bfe6
c4explosive <ao946391@gmail.com>
2015-05-12 19:28:53 -0500

    gotoxy().info

--- PROY1_PR.C
+++ PROY1_PR.C
@@ -11,21 +11,24 @@ void main()
     clrscr();
     do
     {
-	gotoxy(34,2); printf("MUNICIPIO DE PANAMµ");
-	gotoxy(26,3); printf("INFORME DE PLACAS VEHICULARES");
+	gotoxy(32,2); printf("MUNICIPIO DE PANAMµ");
+	gotoxy(27,3); printf("INFORME DE PLACAS VEHICULARES");
 	/*******************HORA******************************/
-	    gotoxy(1,4);
-	    lt=time(NULL);
-	    printf(ctime(&lt));
-	    //time();
+	gotoxy(1,4);
+	lt=time(NULL);
+	printf(ctime(&lt));
 	/*******************ENCABEZADO*************************************/
 	gotoxy(5,6); printf("NOMBRE DEL");gotoxy(5,7); printf("PROPIETARIO");
 	gotoxy(23,6); printf("TIPO");
 	gotoxy(34,6); printf("DESCRIPCI¢N");
 	gotoxy(50,6); printf("NÈMERO DE");gotoxy(52,7); printf("PLACA");
 	gotoxy(65,6); printf("CANTIDAD");gotoxy(66,7); printf("PAGADA");
-	/********************************************************************/
-	gotoxy(6,9);printf("Prueba");
+	/*******************INFO GENERAL***********************************/
+	gotoxy(5,20);printf("CANTIDAD DE PLACA COMERCIAL: ");
+	gotoxy(5,21);printf("CANTIDAD DE PLACA TAXI: ");
+	gotoxy(5,22);printf("CANTIDAD DE PLACA DE BUSES: ");
+	gotoxy(5,23);printf("CANTIDAD DE PLACA PARTICULARES: ");
+	gotoxy(5,24);printf("TOTAL RECOLECTADO: ");
 	getch();
 	op=0;
     } while(op);
